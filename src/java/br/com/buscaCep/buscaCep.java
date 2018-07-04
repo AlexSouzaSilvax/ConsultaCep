5d2b2047cc654e1bf17e83f65d9ce999b20756eb
rayssacoosta <rayssa@crossystem.com.br>
2018-07-04 16:09:03 -0300

    1

---
+++ src/java/br/com/buscaCep/buscaCep.java
@@ -0,0 +1,30 @@
+package br.com.buscaCep;
+
+import javax.swing.JOptionPane;
+
+/**
+ *
+ * @author alex
+ */
+public class buscaCep {
+
+    public static void main(String[] args) {
+
+        // WebServiceCep webServiceCep = WebServiceCep.searchCep("Seu cep");
+        WebServiceCep webServiceCep = WebServiceCep.searchCep(JOptionPane.showInputDialog("Entre com seu Cep:"));
+
+        if (webServiceCep.wasSuccessful()) {
+
+            JOptionPane.showMessageDialog(null, "Cep: " + webServiceCep.getCep()
+                    + "\nLogradouro: " + webServiceCep.getLogradouroFull()
+                    + "\nBairro: " + webServiceCep.getBairro()
+                    + "\nCidade: "
+                    + webServiceCep.getCidade() + " - " + webServiceCep.getUf());
+
+        } else {
+            JOptionPane.showMessageDialog(null, "Erro numero: " + webServiceCep.getResulCode());
+
+            JOptionPane.showMessageDialog(null, "Descriçao do erro: " + webServiceCep.getResultText());
+        }
+    }
+}
